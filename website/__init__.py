d5961a17d04c0506d25441e4daba27987f90d2af
Erik Mattfolk <erik.mattfolk@gmail.com>
2019-08-26 10:38:22 +0200

    Fix language on cheat page

--- website/__init__.py
+++ website/__init__.py
@@ -14,7 +14,7 @@ navigation = [
         (("Start", "Start"),           "",             True),
         (("Inlägg", "Posts"),          "/posts",       True),
         (("Kontakt", "Contact us"),    "/contact",     True),
-        (("Fusk", "Cheats"),           "/cheats",     True),
+        (("Fusk", "Cheats"),           "/cheats",      True),
         (("Vår Github", "Our Github"), GITHUB_LINK,    False),
         (("Stadgar", "By-laws"),       BYLAWS_LINK,    False),
         (("Protokoll", "Protocols"),   PROTOCOLS_LINK, False)]
@@ -57,16 +57,6 @@ def posts_en():
     """ English Posts page """
     return render_page("website/pages/posts_en.md", 1, False)
 
-@app.route("/cheats/se/")
-def cheats_se():
-    """ Swedish Contact page """
-    return render_page("website/pages/cheats_se.md", 2, True)
-
-@app.route("/cheats/en/")
-def cheats_en():
-    """ English Contact page """
-    return render_page("website/pages/cheats_en.md", 2, True)
-
 @app.route("/contact/se/")
 def contact_se():
     """ Swedish Contact page """
@@ -76,6 +66,16 @@ def contact_se():
 def contact_en():
     """ English Contact page """
     return render_page("website/pages/contact_en.md", 2, False)
+
+@app.route("/cheats/se/")
+def cheats_se():
+    """ Swedish Contact page """
+    return render_page("website/pages/cheats_se.md", 3, True)
+
+@app.route("/cheats/en/")
+def cheats_en():
+    """ English Contact page """
+    return render_page("website/pages/cheats_en.md", 3, False)
 
 @app.errorhandler(404)
 def not_found(e):
