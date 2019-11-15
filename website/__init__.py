78561e007d1a4c8d27ff84d587026420960cbbe3
Erik Mattfolk <erik.mattfolk@gmail.com>
2019-11-15 20:17:31 +0100

    Hopefully fix language switching

--- website/__init__.py
+++ website/__init__.py
@@ -9,27 +9,30 @@ BYLAWS_LINK = "https://github.com/lithekod/bylaws/blob/master/stadgar.pdf"
 PROTOCOLS_LINK = "https://github.com/lithekod/protocols"
 
 # Elements used to create Navigation bar
-# ((Name_se, Name_en), Url, Multi-Language?)
+# ((Name_se, Name_en), Url, Internal?)
 navigation = [
-        (("Start", "Start"),            "",              True),
-        (("Inlägg", "Posts"),           "/posts",        True),
-        (("Kontakt", "Contact us"),     "/contact",      True),
-        (("AoC", "AoC"),                "/aoc",          True),
-        (("Tävlingar", "Competitions"), "/competitions", True),
-        (("Fusk", "Cheats"),            "/cheats",       True),
+        (("Start", "Start"),            "/",              True),
+        (("Inlägg", "Posts"),           "/posts/",        True),
+        (("Kontakt", "Contact us"),     "/contact/",      True),
+        (("AoC", "AoC"),                "/aoc/",          True),
+        (("Tävlingar", "Competitions"), "/competitions/", True),
+        (("Fusk", "Cheats"),            "/cheats/",       True),
         (("Vår Github", "Our Github"),  GITHUB_LINK,     False),
         (("Stadgar", "By-laws"),        BYLAWS_LINK,     False),
         (("Protokoll", "Protocols"),    PROTOCOLS_LINK,  False)]
 
-def render_page(path, nav_index, swedish):
+def render_page(path, url, nav_index, swedish):
     """Render a Markdown file into a page on the website
+
     Arguments:
-    path ------- Path to a markdown file (or not.. idk)
-    nav_index -- Index of page in navigation list
-    swedish ---- Whether the page is in swedish or not (english)
+    path - Path to a markdown file.
+    url - The url to the page.
+    nav_index - Index of page in navigation list.
+    swedish - Whether the page is in swedish or not (english).
     """
     return render_template("page.html",
             html=markdown2.markdown_path(path),
+            url=url,
             navigation=navigation,
             selected=nav_index,
             swedish=swedish)
@@ -73,62 +76,62 @@ from the sidebar to highlight.
 @app.route("/se/")
 def index_se():
     """ Swedish Index page """
-    return render_page("website/pages/index_se.md", 0, True)
+    return render_page("website/pages/index_se.md", "/", 0, True)
 
 @app.route("/en/")
 def index_en():
     """ English Index page """
-    return render_page("website/pages/index_en.md", 0, False)
+    return render_page("website/pages/index_en.md", "/", 0, False)
 
 @app.route("/posts/se/")
 def posts_se():
     """ Swedish Posts page """
-    return render_page("website/pages/posts_se.md", 1, True)
+    return render_page("website/pages/posts_se.md", "/posts/", 1, True)
 
 @app.route("/posts/en/")
 def posts_en():
     """ English Posts page """
-    return render_page("website/pages/posts_en.md", 1, False)
+    return render_page("website/pages/posts_en.md", "/posts/", 1, False)
 
 @app.route("/contact/se/")
 def contact_se():
     """ Swedish Contact page """
-    return render_page("website/pages/contact_se.md", 2, True)
+    return render_page("website/pages/contact_se.md", "/contact/", 2, True)
 
 @app.route("/contact/en/")
 def contact_en():
     """ English Contact page """
-    return render_page("website/pages/contact_en.md", 2, False)
+    return render_page("website/pages/contact_en.md", "/contact/", 2, False)
 
 @app.route("/aoc/se/")
 def aoc_se():
     """ Swedish Advent of Code page """
-    return render_page("website/pages/aoc_se.md", 3, True)
+    return render_page("website/pages/aoc_se.md", "/aoc/", 3, True)
 
 @app.route("/aoc/en/")
 def aoc_en():
     """ English Advent of Code page """
-    return render_page("website/pages/aoc_en.md", 3, False)
+    return render_page("website/pages/aoc_en.md", "/aoc/", 3, False)
 
 @app.route("/competitions/se/")
 def competitions_se():
     """ Swedish Competitons page """
-    return render_page("website/pages/competitions_se.md", 4, True)
+    return render_page("website/pages/competitions_se.md", "/competitions/", 4, True)
 
 @app.route("/competitions/en/")
 def competitions_en():
     """ English Competitons page """
-    return render_page("website/pages/competitions_en.md", 4, False)
+    return render_page("website/pages/competitions_en.md", "/competitions/", 4, False)
 
 @app.route("/cheats/se/")
 def cheats_se():
     """ Swedish Cheats page """
-    return render_page("website/pages/cheats_se.md", 5, True)
+    return render_page("website/pages/cheats_se.md", "/cheats/", 5, True)
 
 @app.route("/cheats/en/")
 def cheats_en():
     """ English Cheats page """
-    return render_page("website/pages/cheats_en.md", 5, False)
+    return render_page("website/pages/cheats_en.md", "/cheats/", 5, False)
 
 
 """Other pages
@@ -140,22 +143,22 @@ which results in no page being highlighted.
 @app.route("/ncpc/se/")
 def ncpc_se():
     """ Swedish NCPC page """
-    return render_page("website/pages/ncpc_se.md", -1, True)
+    return render_page("website/pages/ncpc_se.md", "/ncpc/", -1, True)
 
 @app.route("/ncpc/en/")
 def ncpc_en():
     """ English NCPC page """
-    return render_page("website/pages/ncpc_en.md", -1, False)
+    return render_page("website/pages/ncpc_en.md", "/ncpc/", -1, False)
 
 @app.route("/impa/se/")
 def impa_se():
     """ Swedish IMPA page """
-    return render_page("website/pages/impa_se.md", -1, True)
+    return render_page("website/pages/impa_se.md", "/impa/", -1, True)
 
 @app.route("/impa/en/")
 def impa_en():
     """ English IMPA page """
-    return render_page("website/pages/impa_en.md", -1, False)
+    return render_page("website/pages/impa_en.md", "/impa/", -1, False)
 
 
 """Errorhandlers
@@ -165,4 +168,4 @@ For now we only handle pages that are not found.
 @app.errorhandler(404)
 def not_found(e):
     """ 404 Page """
-    return render_page("website/pages/404.md", -1, False), 404
+    return render_page("website/pages/404.md", "/404/", -1, False), 404
