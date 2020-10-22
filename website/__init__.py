0838d5476617f0c65cec0625f46a2d2ce9f551fe
Erik Mattfolk <erik.mattfolk@gmail.com>
2020-10-22 18:30:40 +0200

    Add automatic sidebar index generation

--- website/__init__.py
+++ website/__init__.py
@@ -8,23 +8,24 @@ app = Flask(__name__, static_folder='static')
 # Elements used to create navigation bar
 # ((Name_se, Name_en), Url, Internal?)
 navigation = [
-        (("Start", "Start"),               "/",              True),
-        (("Kontakt", "Contact us"),        "/contact/",      True),
-        (("Tävlingar", "Competitions"),    "/competitions/", True),
-        (("Game Jam", "Game Jam"),         "/gamejam/",      True),
-        (("Organisation", "Organization"), "/organization/", True),
-        (("Fusk", "Cheats"),               "/cheats/",       True)]
+    (("Start", "Start"),               "/",              True),
+    (("Kontakt", "Contact us"),        "/contact/",      True),
+    (("Tävlingar", "Competitions"),    "/competitions/", True),
+    (("Game Jam", "Game Jam"),         "/gamejam/",      True),
+    (("Organisation", "Organization"), "/organization/", True),
+    (("Fusk", "Cheats"),               "/cheats/",       True),
+]
 
 
-def render_page(path, url, nav_index, swedish, injection=""):
+def render_page(path, url, swedish, injection=""):
     """Render a Markdown file into a page on the website.
 
     Arguments:
     path - Path to a markdown file.
     url - The url to the page.
-    nav_index - Index of page in navigation list.
     swedish - Whether the page is in swedish or not (english).
     """
+    nav_index = next((i for i, (_, u, _) in enumerate(navigation) if u == url), -1)
     return render_template("page.html",
             html=markdown2.markdown_path(path),
             injection=injection,
@@ -122,60 +123,60 @@ from the sidebar to highlight.
 @app.route("/se/")
 def index_se():
     """ Swedish Index page """
-    return render_page("website/pages/index_se.md", "/", 0, True)
+    return render_page("website/pages/index_se.md", "/", True)
 
 @app.route("/en/")
 def index_en():
     """ English Index page """
-    return render_page("website/pages/index_en.md", "/", 0, False)
+    return render_page("website/pages/index_en.md", "/", False)
 
 @app.route("/contact/se/")
 def contact_se():
     """ Swedish Contact page """
-    return render_page("website/pages/contact_se.md", "/contact/", 1, True)
+    return render_page("website/pages/contact_se.md", "/contact/", True)
 
 @app.route("/contact/en/")
 def contact_en():
     """ English Contact page """
-    return render_page("website/pages/contact_en.md", "/contact/", 1, False)
+    return render_page("website/pages/contact_en.md", "/contact/", False)
 
 @app.route("/competitions/se/")
 def competitions_se():
     """ Swedish Competitons page """
-    return render_page("website/pages/competitions_se.md", "/competitions/", 2, True)
+    return render_page("website/pages/competitions_se.md", "/competitions/", True)
 
 @app.route("/competitions/en/")
 def competitions_en():
     """ English Competitons page """
-    return render_page("website/pages/competitions_en.md", "/competitions/", 2, False)
+    return render_page("website/pages/competitions_en.md", "/competitions/", False)
 
 @app.route("/gamejam/se/")
 def gamejam_se():
-    return render_page("website/pages/gamejam_se.md", "/gamejam/", 3, True)
+    return render_page("website/pages/gamejam_se.md", "/gamejam/", True)
 
 @app.route("/gamejam/en/")
 def gamejam_en():
-    return render_page("website/pages/gamejam_en.md", "/gamejam/", 3, False)
+    return render_page("website/pages/gamejam_en.md", "/gamejam/", False)
 
 @app.route("/organization/se/")
 def organization_se():
     """ Swedish Organization page """
-    return render_page("website/pages/organization_se.md", "/organization/", 4, True)
+    return render_page("website/pages/organization_se.md", "/organization/", True)
 
 @app.route("/organization/en/")
 def organization_en():
     """ English Organization page """
-    return render_page("website/pages/organization_en.md", "/organization/", 4, False)
+    return render_page("website/pages/organization_en.md", "/organization/", False)
 
 @app.route("/cheats/se/")
 def cheats_se():
     """ Swedish Cheats page """
-    return render_page("website/pages/cheats_se.md", "/cheats/", 5, True)
+    return render_page("website/pages/cheats_se.md", "/cheats/", True)
 
 @app.route("/cheats/en/")
 def cheats_en():
     """ English Cheats page """
-    return render_page("website/pages/cheats_en.md", "/cheats/", 5, False)
+    return render_page("website/pages/cheats_en.md", "/cheats/", False)
 
 """Other pages
 These pages can be accessed from a direct link. They do not show up
@@ -186,54 +187,64 @@ which results in no page being highlighted on the sidebar.
 @app.route("/ncpc/se/")
 def ncpc_se():
     """ Swedish NCPC page """
-    return render_page("website/pages/ncpc_se.md", "/ncpc/", -1, True)
+    return render_page("website/pages/ncpc_se.md", "/ncpc/", True)
 
 @app.route("/ncpc/en/")
 def ncpc_en():
     """ English NCPC page """
-    return render_page("website/pages/ncpc_en.md", "/ncpc/", -1, False)
+    return render_page("website/pages/ncpc_en.md", "/ncpc/", False)
 
 @app.route("/impa/se/")
 def impa_se():
     """ Swedish IMPA page """
-    return render_page("website/pages/impa_se.md", "/impa/", -1, True)
+    return render_page("website/pages/impa_se.md", "/impa/", True)
 
 @app.route("/impa/en/")
 def impa_en():
     """ English IMPA page """
-    return render_page("website/pages/impa_en.md", "/impa/", -1, False)
+    return render_page("website/pages/impa_en.md", "/impa/", False)
 
 @app.route("/aoc/se/")
 def aoc_se():
     """ Swedish Advent of Code page """
-    return render_page("website/pages/aoc_se.md", "/aoc/", -1, True,
+    return render_page("website/pages/aoc_se.md", "/aoc/", True,
                        injection=aoc_standings())
 
 @app.route("/aoc/en/")
 def aoc_en():
     """ English Advent of Code page """
-    return render_page("website/pages/aoc_en.md", "/aoc/", -1, False,
+    return render_page("website/pages/aoc_en.md", "/aoc/", False,
                        injection=aoc_standings())
 
 @app.route("/codingcup/se/")
 def codingcup_se():
     """ Swedish Coding Cup page """
-    return render_page("website/pages/codingcup_se.md", "/codingcup/", -1, True)
+    return render_page("website/pages/codingcup_se.md", "/codingcup/", True)
 
 @app.route("/codingcup/en/")
 def codingcup_en():
     """ English Coding Cup page """
-    return render_page("website/pages/codingcup_en.md", "/codingcup/", -1, False)
+    return render_page("website/pages/codingcup_en.md", "/codingcup/", False)
 
 @app.route("/microjam/se/")
 def microjam_se():
     """ Swedish microjam page """
-    return render_page("website/pages/microjam_se.md", "/microjam/", -1, True)
+    return render_page("website/pages/microjam_se.md", "/microjam/", True)
 
 @app.route("/microjam/en/")
 def microjam_en():
     """ English microjam page """
-    return render_page("website/pages/microjam_en.md", "/microjam/", -1, False)
+    return render_page("website/pages/microjam_en.md", "/microjam/", False)
+
+@app.route("/meetings/se/")
+def meetings_se():
+    """Swedish meetings page"""
+    return render_page("website/pages/meetings_se.md", "/meetings/", True)
+
+@app.route("/meetings/en/")
+def meetings_en():
+    """Enligsh meetings page"""
+    return render_page("website/pages/meetings_en.md", "/meetings/", False)
 
 @app.route("/gitcheatsheet/")
 def gitcheatsheet():
@@ -254,16 +265,6 @@ def emacs():
 def lacc():
     """ LiTHe kod's Amazing Coding Challenges """
     return static_page("website/other/lacc.html")
-
-@app.route("/meetings/se/")
-def meetings_se():
-    """Swedish meetings page"""
-    return render_page("website/pages/meetings_se.md", "/meetings/", -1, True)
-
-@app.route("/meetings/en/")
-def meetings_en():
-    """Enligsh meetings page"""
-    return render_page("website/pages/meetings_en.md", "/meetings/", -1, False)
 
 """ Redirects from old pages.
 These pages used to exist but not anymore. They redirect to the new content
@@ -308,12 +309,12 @@ For now we only handle pages that are not found.
 @app.errorhandler(404)
 def not_found(e):
     """ 404 Page """
-    return render_page("website/pages/404.md", "/404/", -1, False), 404
+    return render_page("website/pages/404.md", "/404/", False), 404
 
 @app.route("/404.html")
 def not_found_gh_pages():
     """ 404 page to please GitHub pages """
-    return render_page("website/pages/404.md", "/404/", -1, False)
+    return render_page("website/pages/404.md", "/404/", False)
 
 
 if __name__ == "__main__":
